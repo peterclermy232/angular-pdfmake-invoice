5f6d5a1485d558e24f1780341ebb1b0e6c9ca154
peterclermy232 <peterclermy232@gmail.com>
2023-09-27 23:39:46 +0300

    invoice

---
+++ src/app/app.component.ts
@@ -0,0 +1,127 @@
+import { Component } from '@angular/core';
+
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+class Product{
+  name: string;
+  price: number;
+  qty: number;
+}
+class Invoice{
+  customerName: string;
+  address: string;
+  contactNo: number;
+  email: string;
+  
+  products: Product[] = [];
+  additionalDetails: string;
+
+  constructor(){
+    // Initially one empty product row we will show 
+    this.products.push(new Product());
+  }
+}
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  invoice = new Invoice(); 
+  
+  generatePDF(action = 'open') {
+    let docDefinition = {
+      content: [
+        {
+          text: 'INVOICE',
+          fontSize: 16,
+          alignment: 'center',
+          color: '#047886'
+        },
+       
+        {
+          text: 'Buyer Details',
+          style: 'sectionHeader'
+        },
+        {
+          columns: [
+            [
+              {
+                text: this.invoice.customerName,
+                bold:true
+              },
+              { text: this.invoice.address },
+              { text: this.invoice.email },
+              { text: this.invoice.contactNo }
+            ],
+            [
+              {
+                text: `Date: ${new Date().toLocaleString()}`,
+                alignment: 'right'
+              },
+              { 
+                text: `Bill No : ${((Math.random() *1000).toFixed(0))}`,
+                alignment: 'right'
+              }
+            ]
+          ]
+        },
+        {
+          text: 'Order Details',
+          style: 'sectionHeader'
+        },
+        {
+          table: {
+            headerRows: 1,
+            widths: ['*', 'auto', 'auto', 'auto'],
+            body: [
+              ['Product', 'Price', 'Quantity', 'Amount'],
+              ...this.invoice.products.map(p => ([p.name, p.price, p.qty, (p.price*p.qty).toFixed(2)])),
+              [{text: 'Total Amount', colSpan: 3}, {}, {}, this.invoice.products.reduce((sum, p)=> sum + (p.qty * p.price), 0).toFixed(2)]
+            ]
+          }
+        },
+        {
+          text: 'Additional Details',
+          style: 'sectionHeader'
+        },
+        {
+            text: this.invoice.additionalDetails,
+            margin: [0, 0 ,0, 15]          
+        },
+        
+        {
+          text: 'Terms and Conditions',
+          style: 'sectionHeader'
+        },
+        {
+          
+        }
+      ],
+      styles: {
+        sectionHeader: {
+          bold: true,
+          decoration: 'underline',
+          fontSize: 14,
+          margin: [0, 15,0, 15]          
+        }
+      }
+    };
+
+    if(action==='download'){
+      pdfMake.createPdf(docDefinition).download();
+    }else if(action === 'print'){
+      pdfMake.createPdf(docDefinition).print();      
+    }else{
+      pdfMake.createPdf(docDefinition).open();      
+    }
+
+  }
+
+  addProduct(){
+    this.invoice.products.push(new Product());
+  }
+  
+}
